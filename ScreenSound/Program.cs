84e8d2555435b284300ff0fab74444cfea066e4f
João Vitor R. da Silva <joaovitor.r.silva.3@gmail.com>
2025-04-26 13:39:33 -0300

    Melhora tratamento de exceções no menu de opções
    O método `ExibeMenuOpcoes` agora inclui um bloco `try-catch` para lidar com entradas inválidas ao converter a opção do usuário. Se a entrada não for um número, uma mensagem de erro é exibida e o menu é mostrado novamente. O código das opções foi reorganizado dentro do bloco `try`, e um bloco `finally` foi adicionado para garantir que o menu seja exibido após a execução, independentemente de erros.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -28,29 +28,47 @@ void ExibeMenuOpcoes()
     Console.WriteLine("[5] Para sair");
 
     Console.Write("\nEscolha uma opção: ");
-    int opcao = int.Parse(Console.ReadLine()!);
-    
-    switch(opcao)
+
+    try
     {
-        case 1:
-            Console.WriteLine("Registrar uma banda");
-            break;
-        case 2:
-            Console.WriteLine("Mostrar todas as bandas");
-            break;
-        case 3:
-            Console.WriteLine("Avaliar uma banda");
-            break;
-        case 4:
-            Console.WriteLine("Exibir média de avaliação da banda");
-            break;
-        case 5:
-            Environment.Exit(0);
-            break;
-        default:
-            Console.WriteLine("Opção inválida.");
-            break;
+        int opcao = int.Parse(Console.ReadLine()!);
+
+        switch (opcao)
+        {
+            case 1:
+                Console.WriteLine("Registrar uma banda");
+                break;
+            case 2:
+                Console.WriteLine("Mostrar todas as bandas");
+                break;
+            case 3:
+                Console.WriteLine("Avaliar uma banda");
+                break;
+            case 4:
+                Console.WriteLine("Exibir média de avaliação da banda");
+                break;
+            case 5:
+                Console.WriteLine("Saindo...");
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Opção inválida.");
+                break;
+        }
     }
+    catch(FormatException)
+    {
+        Console.WriteLine("Formato inválido. Por favor informe apenas números (1-5). Tente novamente.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro: {ex.Message}");
+    }
+    finally 
+    {
+        ExibeMenuOpcoes();
+    }
+    
 }
 
 ExibeScreenSound();
