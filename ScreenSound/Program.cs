0ada7bf5e65b7df0df9711ad81d0ccf4f8eb04ac
João Vitor R. da Silva <joaovitor.r.silva.3@gmail.com>
2025-04-26 13:08:14 -0300

    Adiciona .editorconfig e atualiza Program.cs
    Inclui um novo arquivo `.editorconfig` para silenciar o aviso CS8600. Adiciona um projeto para o arquivo `.editorconfig` na solução. Atualiza a mensagem de boas-vindas no `Program.cs` e implementa as funções `ExibeScreenSound` e `ExibeMenuOpcoes` para melhorar a interação do usuário.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,3 +1,57 @@
-﻿string msg = "Hello World";
+﻿
+string msg = " ScreenSound v1.0.0\n\n" +
+            "A simple program to play sound on screen events.\n" +
+            "Press 'q' to quit.\n";
 
-Console.WriteLine(msg);
+
+void ExibeScreenSound()
+{
+    Console.Clear();
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(msg);
+}   
+
+void ExibeMenuOpcoes()
+{
+    Console.WriteLine("[1] Registrar uma banda.");
+    Console.WriteLine("[2] Mostrar todas as bandas.");
+    Console.WriteLine("[3] Avaliar uma banda");
+    Console.WriteLine("[4] Exibir média de avalição da banda");
+    Console.WriteLine("[5] Para sair");
+
+    Console.Write("\nEscolha uma opção: ");
+    int opcao = int.Parse(Console.ReadLine()!);
+    
+    switch(opcao)
+    {
+        case 1:
+            Console.WriteLine("Registrar uma banda");
+            break;
+        case 2:
+            Console.WriteLine("Mostrar todas as bandas");
+            break;
+        case 3:
+            Console.WriteLine("Avaliar uma banda");
+            break;
+        case 4:
+            Console.WriteLine("Exibir média de avaliação da banda");
+            break;
+        case 5:
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Opção inválida.");
+            break;
+    }
+}
+
+ExibeScreenSound();
+ExibeMenuOpcoes();
